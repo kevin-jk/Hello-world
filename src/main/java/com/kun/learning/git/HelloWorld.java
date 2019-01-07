7a932f54fc3dadb253ac4a490a5faa0ad032aed9
kevin-jk <jiakunonly@gmail.com>
2019-01-07 16:27:17 +0800

    a

---
+++ src/main/java/com/kun/learning/git/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.kun.learning.git;
+
+/**
+ * Created by jrjiakun on 2018/1/7
+ */
+public class HelloWorld {
+    public static void main(String[]arsg){
+        System.out.println("Hello git.");
+    }
+}
